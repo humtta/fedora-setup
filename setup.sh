be352129b82a647e8795c65905b83b6503f0413f
Hugo Marotta <humtta@proton.me>
2026-09-08 23:50:45 -0300

    Install docker

--- setup.sh
+++ setup.sh
@@ -88,3 +88,12 @@ curl -fsSL https://install.determinate.systems/nix | sudo bash -s -- install --n
 
 # Install Devbox
 curl -fsSL https://get.jetify.com/devbox | sudo bash -s -- -f
+
+# Install Docker
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install -y \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin
