bd6951686ff82516818358a573ea12d020478d3a
Hugo Marotta <humtta@proton.me>
2026-09-08 22:45:21 -0300

    Check OS compatibility

--- setup.sh
+++ setup.sh
@@ -4,3 +4,15 @@ set -eo pipefail
 
 # Load OS release information
 source /etc/os-release
+
+# Check OS compatibility
+REQUIRED_VERSION='44'
+
+if [[
+	"${ID}" != 'fedora' || \
+	"${VARIANT_ID}" != 'workstation' || \
+	"${VERSION_ID}" != "${REQUIRED_VERSION}"
+]]; then
+	echo "Error: this script is intended for Fedora Workstation ${REQUIRED_VERSION} only" >&2
+	exit 1
+fi
