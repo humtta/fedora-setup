f07a9a0527082f76dcad6fba4d23799b42dc6e36
Hugo Marotta <humtta@proton.me>
2026-09-08 23:45:07 -0300

    Install vscode

--- setup.sh
+++ setup.sh
@@ -64,3 +64,17 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 
 # Update installed packages
 sudo dnf update -y --refresh
+
+# Install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sudo tee /etc/yum.repos.d/vscode.repo <<-'EOF' >/dev/null
+	[code]
+	name=Visual Studio Code
+	baseurl=https://packages.microsoft.com/yumrepos/vscode
+	enabled=1
+	gpgcheck=1
+	gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo dnf install -y code
